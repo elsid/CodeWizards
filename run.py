a316386afd34e24b67ff5164b9db6c0c9605ffb4
elsid <elsid@yandex-team.ru>
2016-11-23 09:44:59 +0300

    Generate local runner config from template

--- run.py
+++ run.py
@@ -3,13 +3,14 @@
 import strategy
 
 from time import sleep
+from sys import argv
 
 from auto_kill_process import AutoKillProcess
 
 
 if __name__ == '__main__':
     runner = ['java', '-Xms512m', '-Xmx2G', '-server', '-jar',
-              'local-runner-ru/local-runner.jar', 'local-runner.properties']
+              'local-runner-ru/local-runner.jar', argv[1]]
     with AutoKillProcess(runner):
         sleep(3)
         strategy.run()
--- run.sh
+++ run.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 
-LOG=log/run.$(date +%s).log
-./run.py | tee ${LOG}
-echo "log: ${LOG}"
-cat result.txt
+set -ex
+
+if ! [[ "${ID}" ]]; then
+    ID=$(date +%s)
+fi
+
+if ! [[ "${RENDER_TO_SCREEN}" ]]; then
+    RENDER_TO_SCREEN=false
+fi
+
+if ! [[ "${RENDER_TO_SCREEN_SYNC}" ]]; then
+    RENDER_TO_SCREEN_SYNC=false
+fi
+
+mkdir -p log/
+mkdir log/${ID}/
+
+RUN_LOG=log/${ID}/run.log
+GAME_LOG=log/${ID}/game.log
+RESULT_LOG=log/${ID}/result.log
+CONFIG=log/${ID}/local-runner.properties
+
+cp local-runner.properties.tmpl ${CONFIG}
+
+sed -i "s;{{ID}};${ID};g" ${CONFIG}
+sed -i "s;{{RENDER_TO_SCREEN}};${RENDER_TO_SCREEN};g" ${CONFIG}
+sed -i "s;{{RENDER_TO_SCREEN_SYNC}};${RENDER_TO_SCREEN_SYNC};g" ${CONFIG}
+sed -i "s;{{SEED}};${SEED};g" ${CONFIG}
+
+./run.py ${CONFIG} | tee ${RUN_LOG}
+
+echo "run log: ${RUN_LOG}"
