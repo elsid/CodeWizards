fcd09265b8cf2f1e4f3a0770107fbb7783e17a30
elsid <elsid@yandex-team.ru>
2016-11-23 10:18:29 +0300

    Update repeat

--- repeat.sh
+++ repeat.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-LOG=log/run.$(date +%s).log
+ID=$(date +%s)
+
+mkdir -p log/
+mkdir log/${ID}
+
+LOG=log/${ID}/run.log
+
 ./repeat.py "${@}" | tee ${LOG}
+
 echo "log: ${LOG}"
