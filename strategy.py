057745b94ee0a6c8625d2e30a154080883aa8066
elsid <elsid@yandex-team.ru>
2016-11-24 12:19:02 +0300

    CProfile with sort option

--- strategy.py
+++ strategy.py
@@ -4,8 +4,9 @@ from auto_kill_process import AutoKillProcess
 
 
 def run():
-    if environ.get('CPROFILE') == '1':
-        args = ['python3', '-m', 'cProfile', '-s', 'tottime', 'Runner.py']
+    sort = environ.get('CPROFILE')
+    if sort:
+        args = ['python3', '-m', 'cProfile', '-s', (sort if sort != '1' else 'tottime'), 'Runner.py']
     else:
         args = ['python3', 'Runner.py']
     with AutoKillProcess(args, cwd='python3-cgdk', env=environ) as strategy:
