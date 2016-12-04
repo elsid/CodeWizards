60d2479b1782017a605d8c0d7f25038800c52c20
elsid <elsid@yandex-team.ru>
2016-12-04 21:12:45 +0300

    Select target once

--- cpp-cgdk/battle_mode.cpp
+++ cpp-cgdk/battle_mode.cpp
@@ -11,30 +11,13 @@ BattleMode::Result BattleMode::apply(const Context& context) {
 }
 
 void BattleMode::update_target(const Context& context) {
-    const GetAttackRange get_attack_range {context};
-
-    const auto max_distances = {
-        1.3 * context.self().getVisionRange(),
-        context.self().getCastRange() + context.game().getMagicMissileRadius(),
-    };
-    target_ = Target();
     destination_.first = false;
-    for (const auto max_distance : max_distances) {
-        const auto target = get_optimal_target(context, max_distance);
-        const auto unit = target.circular_unit(context.cache());
-        if (!unit) {
-            break;
-        }
-        destination_ = {true, get_optimal_position(context, target, 2 * context.self().getVisionRange(),
-                        OPTIMAL_POSITION_INITIAL_POINTS_COUNT, OPTIMAL_POSITION_MINIMIZE_MAX_FUNCTION_CALLS)};
-        const auto max_attack_range = get_attack_range(context.self()) + unit->getRadius();
-        const auto min_distance = std::min(destination_.second.distance(get_position(*unit)),
-                                           get_position(context.self()).distance(get_position(*unit)));
-        if (min_distance <= max_attack_range) {
-            target_ = target;
-            break;
-        }
+    target_ = get_optimal_target(context, 1.3 * context.self().getVisionRange());
+    if (!target_.is_some()) {
+        return;
     }
+    destination_ = {true, get_optimal_position(context, target_, 2 * context.self().getVisionRange(),
+                    OPTIMAL_POSITION_INITIAL_POINTS_COUNT, OPTIMAL_POSITION_MINIMIZE_MAX_FUNCTION_CALLS)};
 }
 
 }
