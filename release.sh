8bf1c12fc3efa326b23e10b151d5489bde0e46fa
elsid <elsid@yandex-team.ru>
2016-12-10 13:10:03 +0300

    Use clang

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ set -ex
 
 mkdir -p cpp-cgdk-release
 cd cpp-cgdk-release
-cmake -DCMAKE_BUILD_TYPE=Release ../cpp-cgdk
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ ../cpp-cgdk
 make -j$(nproc)
 bin/cpp-cgdk-tests
