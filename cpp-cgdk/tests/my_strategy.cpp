e28969f663c7c693a0b8ca5295371ef71c3b70ef
elsid <elsid@yandex-team.ru>
2016-12-13 21:07:30 +0300

    Revert "Change position penalty"
    This reverts commit d3aa3ce9d1b0cf91baa77c57117a48c74878a768.

--- cpp-cgdk/tests/my_strategy.cpp
+++ cpp-cgdk/tests/my_strategy.cpp
@@ -237,8 +237,8 @@ TEST(MyStrategy, with_near_enemy) {
     MyStrategy().move(self, world, GAME, move);
     EXPECT_DOUBLE_EQ(move.getCastAngle(), 0.024994793618920159);
     EXPECT_DOUBLE_EQ(move.getMinCastDistance(), 355.12498047485116);
-    EXPECT_DOUBLE_EQ(move.getSpeed(), 3.9977153647805173);
-    EXPECT_DOUBLE_EQ(move.getStrafeSpeed(), -0.10138008920023656);
+    EXPECT_DOUBLE_EQ(move.getSpeed(), 3.9948218054794036);
+    EXPECT_DOUBLE_EQ(move.getStrafeSpeed(), 0.15260010693731843);
     EXPECT_DOUBLE_EQ(move.getTurn(), 0.024994793618920159);
     EXPECT_EQ(move.getAction(), model::ACTION_MAGIC_MISSILE);
     EXPECT_EQ(move.getSkillToLearn(), model::_SKILL_UNKNOWN_);
