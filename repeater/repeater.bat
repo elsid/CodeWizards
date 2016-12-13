97c7ecfc3e340c7bc93c591a5757253594e64324
elsid <elsid@yandex-team.ru>
2016-12-13 03:26:59 +0300

    Update submodules

--- repeater/repeater.bat
+++ repeater/repeater.bat
@@ -1,1 +1,18 @@
-start java -Xms128M -Xmx1G -cp ".;*;%~dp0/*" -jar repeater.jar %1
+set JAVA_BIN="
+
+if "%JAVA8_64_HOME%" neq "" (
+    if exist "%JAVA8_64_HOME%\bin\java.exe" (
+        set JAVA_BIN="%JAVA8_64_HOME%\bin\"
+        goto java-start
+    )
+)
+
+if "%JAVA_HOME%" neq "" (
+    if exist "%JAVA_HOME%\bin\java.exe" (
+        set JAVA_BIN="%JAVA_HOME%\bin\"
+        goto java-start
+    )
+)
+
+:java-start
+start "" "%JAVA_BIN:"=%java" -Xms128M -Xmx1G -cp ".;*;%~dp0/*" -jar repeater.jar %1
