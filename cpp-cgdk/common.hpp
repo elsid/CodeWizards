6954725717cb00379ebfcbf9867582ee38179069
elsid <elsid@yandex-team.ru>
2017-01-11 19:03:20 -0700

    Log when ELSID_STRATEGY_DEBUG_LOG is defined

--- cpp-cgdk/common.hpp
+++ cpp-cgdk/common.hpp
@@ -29,7 +29,7 @@ const long OPTIMAL_POSITION_MINIMIZE_MAX_FUNCTION_CALLS = 1000;
 const Tick BATTLE_MODE_TICKS = 2500;
 const Tick TICKS_TO_DEATH_FOR_RETREAT = 150;
 
-#ifdef ELSID_STRATEGY_DEBUG
+#if defined(ELSID_STRATEGY_DEBUG) || defined(ELSID_STRATEGY_DEBUG_LOG)
 
 #define SLOG(context) std::cout << '[' << context.world().getTickIndex() << "] "
 
